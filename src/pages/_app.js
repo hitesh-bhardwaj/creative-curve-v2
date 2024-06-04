fb66553518399807d91c771e6851882848a2c960
Hitesh Bhardwaj <bhardwajhitesh09@gmail.com>
2024-06-04 17:04:38 +0530

    Canvas Conditional rendering

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,26 +1,62 @@
-import "@/styles/globals.css";
+// import "@/styles/globals.css";
+// import Header from '@/components/Header';
+// import Footer from '@/components/Footer';
+// import ReactLenis from '@studio-freight/react-lenis';
+// import dynamic from "next/dynamic";
+
+// const WebGl_Canvas = dynamic(
+//   () => import('@/components/WebglBackground'),
+//   { ssr: false }
+// )
+
+// export default function App({ Component, pageProps }) {
+
+//   return (
+//     <>
+      
+//       <ReactLenis root duration={2}>
+//           <Header />
+//             <Component {...pageProps} />
+//           <Footer />
+//       </ReactLenis>
+
+//       <WebGl_Canvas />
+//     </>
+//   );
+// }
+
+import '@/styles/globals.css';
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
 import ReactLenis from '@studio-freight/react-lenis';
-import dynamic from "next/dynamic";
+import dynamic from 'next/dynamic';
+import { useEffect, useState } from 'react';
 
-const WebGl_Canvas = dynamic(
-  () => import('@/components/WebglBackground'),
-  { ssr: false }
-)
+// Dynamically import the WebGl_Canvas component
+const WebGl_Canvas = dynamic(() => import('@/components/WebglBackground'), { ssr: false });
 
 export default function App({ Component, pageProps }) {
+  const [showWebGL, setShowWebGL] = useState(false);
+
+  useEffect(() => {
+    // Defer the loading of the WebGL component
+    const timer = setTimeout(() => {
+      setShowWebGL(true);
+    }, 3000); // Adjust the delay as needed
+
+    return () => clearTimeout(timer);
+  }, []);
 
   return (
     <>
-      
       <ReactLenis root duration={2}>
-          <Header />
-            <Component {...pageProps} />
-          <Footer />
+        <Header />
+        <Component {...pageProps} />
+        <Footer />
       </ReactLenis>
 
-      <WebGl_Canvas />
+      {showWebGL && <WebGl_Canvas />}
     </>
   );
 }
+
